de9a20a38072cb9cbd4cdd76e1864f2a10de6eb0
Suraj Kumar <suraj.m@williamslea.com>
2026-03-16 00:16:19 +0530

    [feature/initial_setup]: created required folders to keep everything/all-services seperate

---
+++ services/lead-service/src/main/java/com/syncbrand_platform/lead_service/LeadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syncbrand_platform.lead_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadServiceApplication.class, args);
+	}
+
+}
